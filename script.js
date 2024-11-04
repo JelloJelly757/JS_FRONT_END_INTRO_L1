8ff26c6655fab740976ee2fd81faee2b28bc9a1d
Elizabeth <elizabethg2796@tuckahoeschools.org>
2024-11-04 13:28:19 +0000

    intial commit

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function changeTextColor(){
     })
 } 
 changeTextColor(); 
+
+const pictureElement = document.getElementById("toggle-image"); 
+function changePicture(){
+
+
+const images = [
+    "https://www.lohud.com/gcdn/authoring/authoring-images/2024/03/04/PWES/72834051007-jm-030324-girls-c-b-ball-final-47.jpg?width=1838&height=1419&fit=crop&format=pjpg&auto=webp",
+    "https://www.lohud.com/gcdn/authoring/authoring-images/2024/03/04/PWES/72834038007-jm-030324-girls-c-b-ball-final-33.jpg?width=1838&height=1217&fit=crop&format=pjpg&auto=webp", 
+    "https://www.lohud.com/gcdn/authoring/authoring-images/2024/03/04/PWES/72834029007-jm-030324-girls-c-b-ball-final-25.jpg?width=1838&height=1160&fit=crop&format=pjpg&auto=webp"
+]; 
+
+let currentIndex = 0; 
+
+pictureElement.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    pictureElement.src = images[currentIndex]; 
+});
+}
+
+changePicture()
